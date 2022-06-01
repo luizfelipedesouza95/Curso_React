7f3a55bbdc33d4edcd19b88e24285ced31bbf3b0
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-05-31 21:22:58 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Luiz Felipe'
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div id="titulo"><h1>Sorteador</h1>
+        <form id="form">
+            <br></br>
+          <div class="text-center">Sortear    <input type="number" id="qtd_num_sortear"></input> número
+            <br></br>
+            entre <input type="number" id="numInicial"></input> e <input type="number" id="numFinal"></input>
+          </div>
+          <br></br>
+          <div id="div_frase">Sorteador de números aleatórios gratuito!!!</div>
+          <br></br>
+          <div><button type="submit" id="btn_sortear">Sortear Agora!</button></div>
+        </form>
+      </div>
     </div>
   );
 }
